00c59a3ee035f17c8b322eaeb60bd52b9f4637ed
Atul <atul@workplacefabric.com>
2022-05-18 01:04:43 +0530

    POST - Input validation using Joi

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+const Joi = require('joi');
 const express = require('express');
 const app = express();
+
+app.use(express.json());
 
 const cources = [{
         "id": 1,
@@ -29,6 +32,25 @@ app.get('/api/cources/:id', (req, res) => {
         res.status(404).send('The Cource wth the given ID was not found');
     res.send(cource);
 })
+
+app.post('/api/cources', (req, res) => {
+    const schema = Joi.object({
+        name: Joi.string().min(3).required()
+    })
+
+    const result = schema.validate(req.body);
+    if (result.error) {
+        res.status(400).send(result.error.details[0].message)
+        return
+    }
+
+    const cource = {
+        id: cources.length + 1,
+        name: req.body.name
+    };
+    cources.push(cource);
+    res.status(201).send(cource);
+});
 
 const port = process.env.PORT || 3001;
 
